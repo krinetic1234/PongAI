a817c3a06f979a4b45851bfd6288e6d21a8df960
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2023-08-04 20:49:19 -0700

    basic game train logic

--- run.py
+++ run.py
@@ -24,21 +24,62 @@ class PongGame:
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_w]:
-                game.move_paddle(left=True, up=True)
+                self.game.move_paddle(left=True, up=True)
 
             if keys[pygame.K_s]:
-                game.move_paddle(left=True, up=False)
+                self.game.move_paddle(left=True, up=False)
 
 
-            game_info = game.loop()
+            game_info = self.game.loop()
             print(game_info.left_score, game_info.right_score)
-            game.draw(False, True)
+            self.game.draw(False, True)
             pygame.display.update()
 
         pygame.quit()
 
+    def train_ai(self, genome1, genome2, config):
+        net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
+        net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
+
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+
+            output1 = net1.activate((self.left_paddle.y, self.ball.y, abs(self.left_paddle.x-self.ball.x)))
+            output2 = net2.activate((self.right_paddle.y, self.ball.y, abs(self.right_paddle.x-self.ball.x)))
+            print(output1, output2)
+
+            game_info = self.game.loop()
+            self.game.loop()
+            self.game.draw()
+            pygame.display.update()
+
+            if game_info.left_score >= 1 or game_info.right_score >= 1:
+                self.calculate_fitness(genome1, genome2, game_info)
+                break
+        
+    def calculate_fitness(self, genome1, genome2, game_info):
+        pass
+
+
+
 def eval_genomes(genomes, config):
-    pass
+    width, height = 700, 500
+    window = pygame.display.set_mode((width, height))
+
+    # train over each pair of AIs
+    for i, (genome_id1, genome1) in enumerate(genomes):
+        if i == len(genomes) - 1:
+            break
+
+        genome1.fitness = 0
+        for (genome_id2, genome2) in genomes[i+1:]:
+            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+            game = PongGame(window, width, height)
+            game.train_ai(genome1, genome2, config)
+
 
 def run_neat(config):
     # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-xx')
