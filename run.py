b6d82bf36c81d9b673fa51677ebf14f926dabeaa
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2023-08-04 21:09:36 -0700

    game logic works

--- run.py
+++ run.py
@@ -38,30 +38,51 @@ class PongGame:
         pygame.quit()
 
     def train_ai(self, genome1, genome2, config):
+        # create neural nets
         net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
         net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
 
         run = True
         while run:
+            # quit game if user exits
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     quit()
 
             output1 = net1.activate((self.left_paddle.y, self.ball.y, abs(self.left_paddle.x-self.ball.x)))
+            decision1 = output1.index(max(output1))
+            
+            # movements for paddle 1 (left)
+            if decision1 == 0:
+                pass
+            elif decision1 == 1:
+                self.game.move_paddle(left=True, up=True)
+            elif decision1 == 2:
+                self.game.move_paddle(left=True, up=False)
+
             output2 = net2.activate((self.right_paddle.y, self.ball.y, abs(self.right_paddle.x-self.ball.x)))
-            print(output1, output2)
+            decision2 = output2.index(max(output2))
+
+            # movements for paddle 2 (right)
+            if decision2 == 0:
+                pass
+            elif decision2 == 1:
+                self.game.move_paddle(left=False, up=True)
+            elif decision2 == 2:
+                self.game.move_paddle(left=False, up=False)
 
             game_info = self.game.loop()
             self.game.loop()
-            self.game.draw()
+            self.game.draw(draw_score=False, draw_hits=True)
             pygame.display.update()
 
-            if game_info.left_score >= 1 or game_info.right_score >= 1:
+            if game_info.left_score >= 1 or game_info.right_score >= 1 or game_info.left_hits > 50:
                 self.calculate_fitness(genome1, genome2, game_info)
                 break
         
     def calculate_fitness(self, genome1, genome2, game_info):
-        pass
+        genome1.fitness += game_info.left_hits
+        genome2.fitness += game_info.right_hits
 
 
 
@@ -89,7 +110,7 @@ def run_neat(config):
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
-    p.add_reporter(neat.CheckPointer(1))
+    p.add_reporter(neat.Checkpointer(1))
 
     # run fitness function and get winner
     winner = p.run(eval_genomes, 50)
